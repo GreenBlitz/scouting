6df03a820b68f700fa6b89fe019cf168cbc9e9f7
talbor49 <talbor49@gmail.com>
2017-02-03 19:07:43 +0200

    Started events

---
+++ routes/event.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+app.post('/', function(request, response){
+    var query1 = request.body.var1;
+});
+
+module.exports = router;
