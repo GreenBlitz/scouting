35c17fb6a8d3b5a79de0fdbdfbebe73135cf23bd
Daniel Eliad <daniel.eliad32@gmail.com>
2017-02-03 19:37:01 +0200

    Mock of upload file

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var client = require('../connection.js');
+var fs = require('fs');
 var router = express.Router();
 
 /* POST upload page. */
 router.post('/', function(req, res, next) {
+
+    /// .   .   . Insert into ES and get gameid
+
+    var gameid = 0;
+
+    var files = req.files;
+    if (files && files.gameVideo) {
+        var file = files.gameVideo;
+        var oldPath = file.path;
+        var newPath = "../games/" + gameid + ".mp4";
+        fs.rename(oldPath, newPath, function(err, res) {
+            if (err) {
+                console.error("Uploading game #" + gameid, "failed. Please insert video manually as: uploads/" + gameid + ".mp4");
+            } else {
+                console.log("Uploading game #" + gameid, "Successful! :)");
+            }
+        })
+    }
+
     // var body = req.body;
     // var blueTeam = [1, 2, 3];
     // var redTeam = [4, 5, 6];
