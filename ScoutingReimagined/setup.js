9da7fb6b0923c5a08abce7a12e8027bc9f864abf
Daniel Eliad <daniel.eliad32@gmail.com>
2017-02-17 18:26:56 +0200

    Indexed events and mapped all it's types

--- ScoutingReimagined/setup.js
+++ ScoutingReimagined/setup.js
@@ -1,16 +1,46 @@
 function setup() {
     var client = require('./connection');
     indexGames(client);
+    indexEvents(client);
 }
 
+/**
+ * Indexes 'events' and uses @function mapEvents to map all event types
+ * @param  {ElasticsearchConnection} client connection to the elasticsearch instance
+ */
+function indexEvents(client) {
+    client.indices.exists({
+        index: 'events'
+    }, function (err, exists) {
+        if (!exists) {
+            client.indices.create({
+                index: 'events'
+            }, function (err, res) {
+                console.log("err", err);
+                console.log("res", res);
+                if (!err) {
+                    // Push mappings
+                    mapEvents(client);
+                }
+            });
+        } else {
+            mapEvents(client);
+        }
+    });
+}
+
+/**
+ * Indexes 'games' and uses @function mapGames to map the type game
+ * @param  {ElasticsearchConnection} client connection to the elasticsearch instance
+ */
 function indexGames(client) {
     client.indices.exists({
         index: 'games'
-    }, function(err, exists) {
+    }, function (err, exists) {
         if (!exists) {
             client.indices.create({
                 index: 'games'
-            }, function(err, res) {
+            }, function (err, res) {
                 console.log("err", err);
                 console.log("res", res);
                 if (!err) {
@@ -25,31 +55,61 @@ function indexGames(client) {
     });
 }
 
-
+/**
+ * Maps the type game to the index games using schema defined in @file schemas/games.js
+ * @param  {ElasticsearchConnection} client connection to the elasticsearch instance
+ * @return {[type]}        [description]
+ */
 function mapGames(client) {
+    var game = require('./schemas/game');
     client.indices.putMapping({
         index: "games",
         type: "game",
-        body: {
-            properties: {
-                gameId: {
-                    type: "integer"
-                },
-                blueTeams: {
-                    type: "integer"
-                },
-                redTeams: {
-                    type: "integer"
-                },
-                comments: {
-                    type: "text"
-                }
-            }
-        }
-    }, function(err, res) {
+        body: game
+    }, function (err, res) {
         console.log("err: ", err);
         console.log("res: ", res);
     });
+}
+
+/**
+ * Maps every event type (defined in @file schemas/events/*.js) in the index events
+ * @param  {ElasticsearchConnection} client connection to the elasticsearch instance
+ */
+function mapEvents(client) {
+    //	TODO: Check if inserting the require expression inside typeToMapping is possible
+    var ballPickup = require('./schemas/events/ballPickup');
+    var climb = require('./schemas/events/climb');
+    var disabled = require('./schemas/events/disabled');
+    var fight = require('./schemas/events/fight');
+    var gearPickup = require('./schemas/events/gearPickup');
+    var gearPlace = require('./schemas/events/gearPlace');
+    var shooting = require('./schemas/events/shooting');
+
+    /**
+     * Maps the type name to it's mapping
+     * @type {Dictionary}
+     */
+    var typeToMapping = {
+        "ballPickup": ballPickup,
+        "climb": climb,
+        "disabled": disabled,
+        "fight": fight,
+        "gearPickup": gearPickup,
+        "gearPlace": gearPlace,
+        "shooting": shooting,
+    };
+
+    for (var type in typeToMapping) {
+        client.indices.putMapping({
+            index: "events",
+            type: type,
+            body: typeToMapping[type]
+        }, function (err, res) {
+            console.log("err: ", err);
+            console.log("res: ", res);
+        });
+    }
 }
 
 module.exports = {
