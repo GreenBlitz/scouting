2cfee86e726828cbfcf13c912c04ed303de5f474
talbor49 <talbor49@gmail.com>
2017-02-22 23:36:04 +0200

    Events are now successfully sent and saved in ElasticSearch

--- ScoutingReimagined/routes/event.js
+++ ScoutingReimagined/routes/event.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var client = require('../lib/elasticsearch/connection');
 
 /* GET home page. */
 router.post('/', function(req, res){
     console.log('GOT EVENT TO /event!!!! event body: ' + JSON.stringify(req.body));
 
-    res.end();
+    var eventName = req.body['meta_data[eventName]'];
+    var gameId = req.body['meta_data[gameId]'];
+    var teamNumber = req.body['meta_data[teamNumber]'];
+
+
+    client.index({
+        // teams/team/4590 : {}
+        index: 'events',
+        type: eventName,
+        body: eventBodyToEvent(req.body)
+    }, function (err, resp, status) {
+        console.log(resp);
+        if (err) {
+            console.log(err);
+            res.sendStatus(500); // Internal server error
+        } else {
+            /// .   .   .   Insert into ES and get gameid
+            console.log("Successfully wrote to elasticsearch! response: " + resp);
+            res.sendStatus(200);
+        }
+    });
 });
+
+function eventBodyToEvent(bodyEvent) {
+    delete bodyEvent['meta_data[eventName]'];
+    delete bodyEvent['meta_data[gameId]'];
+    bodyEvent.teamNumber = bodyEvent['meta_data[teamNumber]'];
+    delete bodyEvent['meta_data[teamNumber]'];
+    return bodyEvent;
+}
 
 module.exports = router;
