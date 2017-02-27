a7146a71b048e9c69110688e6c85cec7cff9be3c
Daniel Eliad <daniel.eliad32@gmail.com>
2017-02-27 14:46:20 +0200

    Added retention of events through new scouting sessions and new delete button

--- ScoutingReimagined/routes/event.js
+++ ScoutingReimagined/routes/event.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var client = require('../lib/elasticsearch/connection');
 
 /* GET home page. */
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
     console.log('GOT EVENT TO /event!!!! event body: ' + JSON.stringify(req.body));
 
     var event = req.body;
@@ -27,6 +27,101 @@ router.post('/', function(req, res){
             console.log("Successfully wrote to elasticsearch! response: " + resp);
             res.sendStatus(200);
         }
+    });
+});
+
+router.delete('/', function (req, res) {
+    console.log('GOT EVENT DELETE TO /event!!!! event body: ' + JSON.stringify(req.body));
+    var event = req.body;
+    client.deleteByQuery({
+        index: 'events',
+        type: event.eventName,
+        body: {
+            "query": {
+                "constant_score": {
+                    "filter": {
+                        "bool": {
+                            "must": [{
+                                    "term": {
+                                        "startTime": event.startTime
+                                    }
+                                },
+                                {
+                                    "term": {
+                                        "endTime": event.endTime
+                                    }
+                                },
+                                {
+                                    "term": {
+                                        "gameId": event.gameId
+                                    }
+                                },
+                                {
+                                    "term": {
+                                        "teamNumber": event.teamNumber
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    }, function (err, resp) {
+        console.log("here");
+        console.log(resp);
+        console.log("after");
+        if (err) {
+            console.log(err);
+            res.sendStatus(500); // Internal server error
+        } else {
+            console.log("Successfully deleted from elasticsearch! response: " + resp);
+            res.sendStatus(200);
+        }
+    });
+    console.log("after making request");
+});
+
+router.get('/', function (req, res) {
+    client.search({
+        index: 'events',
+        body: {
+            sort: [{
+                startTime: "asc"
+            }],
+            query: {
+                "constant_score": {
+                    "filter": {
+                        "bool": {
+                            "must": [{
+                                    "term": {
+                                        gameId: req.query.gameId
+                                    }
+                                },
+                                {
+                                    "term": {
+                                        teamNumber: req.query.teamNumber
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    }, function (err, resp) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        console.log(resp);
+        var events = [];
+        for (var i = 0; i < resp.hits.hits.length; i++) {
+            var event = resp.hits.hits[i]._source;
+            event.eventName = resp.hits.hits[i]._type;
+            events.push(event);
+        }
+        res.send(events);
     });
 });
 
