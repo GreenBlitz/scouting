e3c0aa35c6aa7b33b775273264bbfab1a76c14d9
talbor49 <talbor49@gmail.com>
2017-03-18 16:01:59 +0200

    Made argument parsing in dumpAllData smarter and made host and port command line parameters

--- dumpAllData.py
+++ dumpAllData.py
@@ -1,7 +1,25 @@
 import datetime, pickle, os
 from elasticsearch import Elasticsearch
+import argparse
 
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+dump_directory = 'db_dumps'
+
+
+parser = argparse.ArgumentParser(prog='dumpAllData.py', description='Reads all data from a scouting database and dumps it into a pickle file')
+parser.add_argument('--host', type=str, default='localhost')
+parser.add_argument('--port', type=int, default=9200)
+parser.add_argument('--filepath', type=str, default=os.path.join(dump_directory, 'db_data_dump_%s.p' % datetime.datetime.now().strftime("%I:%M%p_%B_%d_%Y")))
+
+args = parser.parse_args()
+host = args.host
+port = args.port
+filepath = args.filepath
+
+
+try:
+    es = Elasticsearch([{'host': host, 'port': port}])
+except Exception:
+    sys.exit("Error in connection to database: %s" % str())
 
 MAX_SIZE = 10000
 
@@ -27,12 +45,10 @@ data = { \
     '.kibana': kibana_data\
 }
 
-dump_directory = 'db_dumps'
 if not os.path.exists(dump_directory):
     os.makedirs(dump_directory)
 
-filename = os.path.join(dump_directory, 'db_data_dump_%s.p' % datetime.datetime.now().strftime("%I:%M%p_%B_%d_%Y"))
-with open(filename, 'w+') as dumpfile:
+with open(filepath, 'w+') as dumpfile:
     pickle.dump(data, dumpfile)
 
-print('Successfuly pickled all data into into file %s' % filename)
+print('Successfuly pickled all data into into file %s' % filepath)
