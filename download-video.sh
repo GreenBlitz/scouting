c115190d9131a3ee2e94a555afee8a7c426a5303
Daniel Eliad <daniel.eliad32@gmail.com>
2017-04-13 02:19:28 +0300

    Fixed dowload-video.sh

--- download-video.sh
+++ download-video.sh
@@ -1,2 +1,1 @@
-rm -rf currentGame.mp4;
-ffmpeg -i `youtube-dl -f $1 -g $2` -vcodec copy -flags +global_header currentGame.mp4;
+ffmpeg -i `youtube-dl -f $1 -g $2` -vcodec copy -flags +global_header $3$3$3;
