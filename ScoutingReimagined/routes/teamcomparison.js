a1cc4263cd44bfd46e505cdc32c66aaf9be22ee3
talbor49 <talbor49@gmail.com>
2017-04-22 07:39:09 +0300

    started integrating python into js

--- ScoutingReimagined/routes/teamcomparison.js
+++ ScoutingReimagined/routes/teamcomparison.js
@@ -6,7 +6,42 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     console.log("getting all events..");
     var events = getAllEvents();
-    res.render('teamcomparison', {'events': events});
+    res.render('teamcomparison', {'events': [
+        {
+            "_index": "events",
+            "_type": "gearplace",
+            "_id": "AVrXkyql3SkVUya5L5YR",
+            "_score": 1,
+            "_source": {
+                "status": "Success",
+                "matchPart": "teleop",
+                "gameId": 82,
+                "startTime": 24,
+                "timeTook": 0,
+                "competition": "district4",
+                "location": "Right",
+                "teamNumber": 1943,
+                "endTime": 24
+            }
+        },
+        {
+            "_index": "events",
+            "_type": "gearplace",
+            "_id": "AVrXoboc3SkVUya5L5Zl",
+            "_score": 1,
+            "_source": {
+                "status": "Success",
+                "matchPart": "teleop",
+                "gameId": 83,
+                "startTime": 74,
+                "timeTook": 3,
+                "competition": "district4",
+                "location": "Left",
+                "teamNumber": 1690,
+                "endTime": 77
+            }
+        }
+    ]});
 });
 
 
