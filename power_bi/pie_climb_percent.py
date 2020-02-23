45d66896cc898aeea94df5086cca54dde262befe
TheOnion313 <royn77777@gmail.com>
2020-02-23 10:18:59 +0200

    remade team page climb to unclude failed attempts

--- power_bi/pie_climb_percent.py
+++ power_bi/pie_climb_percent.py
@@ -15,28 +15,30 @@ import team_page
 def main(team_id):
     data = data_manipulation.source_to_list(f"{project_path}/2020Data.xlsx")
     team_data = data_manipulation.by_team(data, team_id)
-    all_games = team_page.get_games(data)
-    climb_data = list(filter(lambda x: x['eventType'] == "Climb" and x['status'] == 'Success', team_data))
+    all_games = team_page.get_games(team_data)
+    climb_data = list(filter(lambda x: x['eventType'] == "Climb", team_data))
     if len(climb_data) > 0:
-        climb_positions = {'0': 0, '1st': 0, '2nd': 0, '3rd': 0}
-        no_climb_games = list(filter(lambda x: x not in team_page.get_games(climb_data), data))
-        climb_positions['0'] += len(no_climb_games)
+        climb_positions = {'did not try': 0, 'tried and failed': 0, 'success': 0}
+        no_climb_games = list(filter(lambda x: x not in team_page.get_games(climb_data), all_games))
+        climb_positions['did not try'] += len(no_climb_games)
         for event in climb_data:
-            if event['When'] == '1st':
-                climb_positions['1st'] += 1
-            if event['When'] == '2st':
-                climb_positions['2st'] += 1
-            if event['When'] == '3st':
-                climb_positions['3st'] += 1
-        colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+            if event['status'] == 'Success':
+                climb_positions['success'] += 1
+            if event['status'] == 'Fail':
+                climb_positions['tried and failed'] += 1
+
+        colors = ['yellowgreen', 'gold', 'lightskyblue']
         plt.pie(list(climb_positions.values()), radius=1.5, labels=list(climb_positions.keys()), colors=colors)
         plt.legend()
         plt.show()
+    elif len(all_games) > 0:
+        plt.pie([1,0], radius=1.5, labels=["did not try", " "])
+        plt.legend()
+        plt.show()
     else:
-        plt.plot([0,0,0], [0,0,0], 'w', label="no data")
+        plt.plot([0, 0, 0], [0, 0, 0], 'w', label='no data')  # writing no data
         plt.rcParams.update({'font.size': 40})
         plt.legend()
         plt.show()
-if __name__ == '__main__':
-    main()
+
 
